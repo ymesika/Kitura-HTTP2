ca5f7ee13fa5a028d99c4666c6739ebce4dde2e0
Yossi Mesika <mesika@il.ibm.com>
2017-06-12 16:50:50 +0300

    Disable OSX build and print curl version

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o verbose
 if [ -n "${DOCKER_IMAGE}" ]; then
     docker pull ${DOCKER_IMAGE}
     docker run --env SWIFT_VERSION -v ${TRAVIS_BUILD_DIR}:${TRAVIS_BUILD_DIR} ${DOCKER_IMAGE} /bin/bash -c "apt-get update && apt-get install -y git sudo lsb-release wget libxml2 && cd $TRAVIS_BUILD_DIR && ./build.sh"
+    echo "<<>>"
+    curl --version
+    openssl version
 else
     test -n "${SWIFT_VERSION}" && echo "${SWIFT_VERSION}" > .swift-version || echo "SWIFT_VERSION not set, using $(cat .swift-version)"
     git clone https://github.com/IBM-Swift/Package-Builder.git
