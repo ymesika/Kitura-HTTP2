50fd4195036872b2d19d7f8d309a10e3b54ea248
Yossi Mesika <mesika@il.ibm.com>
2017-06-14 15:53:18 +0300

    Silence the curl and nghttp2 builds

--- build.sh
+++ build.sh
@@ -9,13 +9,13 @@ sudo apt-get install binutils libcunit1-dev libssl-dev libxml2-dev libev-dev \
 
 # Build nghttp2 from source
 wget https://github.com/nghttp2/nghttp2/releases/download/v1.23.1/nghttp2-1.23.1.tar.bz2
-tar -xvjf nghttp2-1.23.1.tar.bz2
+tar -xjf nghttp2-1.23.1.tar.bz2
 cd nghttp2-1.23.1
 autoreconf -i
 automake
 autoconf
 ./configure --enable-lib-only
-make
+make --quiet
 sudo make install
 cd ..
 
@@ -24,14 +24,14 @@ sudo apt-get install build-essential
 
 # Build curl from source
 wget https://curl.haxx.se/download/curl-7.54.0.tar.bz2
-tar -xvjf curl-7.54.0.tar.bz2
+tar -xjf curl-7.54.0.tar.bz2
 cd curl-7.54.0
 ./configure --with-nghttp2 --prefix=/usr/local
-make
+make --quiet
 sudo make install
 sudo ldconfig
 cd ..
 
 cd ..
- 
+
 curl --version
